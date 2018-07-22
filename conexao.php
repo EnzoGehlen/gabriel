75b97a8b824b914306a5a47b274a5b3b4194fc7b
Enzo <enzo.gehlen@hotmail.com>
2018-07-22 14:49:41 -0300

    teste

--- conexao.php
+++ conexao.php
@@ -1,14 +1,28 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'gabriel';
-$user = 'root';
-$pass = '';
+
+
+$host = 'sql304.epizy.com';
+
+$dbname = 'epiz_22370561_gabriel';
+
+$user = 'epiz_22370561';
+
+$pass = 'eqrkKT1WRXLZlJ';
+
+
 
 $mysqli = new mysqli($host, $user, $pass, $dbname);
+
 if (mysqli_connect_errno())
+
     trigger_error(mysql_connect_error());
 
 
+
+
+
 $mysqli->set_charset("utf8");
+
 date_default_timezone_set('America/Sao_Paulo');
+
